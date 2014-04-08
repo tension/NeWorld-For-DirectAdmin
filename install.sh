54b43f6280dd12a9f53c8499414275fef56db99d
tension <tension@me.com>
2014-04-08 20:07:08 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -1,26 +1,10 @@
-#!/bin/bash
-
-if [ $(id -u) != "0" ]; then
-    printf "错误: 必须使用root权限运行本脚本!\n"
-    exit 1
-fi
-clear
-printf "
-####################################################
-#                                                  #
-# NewWorld For DirectAdmin                         #
-# Version: 1.1                                     #
-# Author: tension                                  #
-# Website: http://tension.name                     #
-# For DirectAdmin                                  #
-#                                                  #
-####################################################
-"
-
 cd /usr/local/directadmin/data/skins/ #进入主题所在目录
-wget https://github.com/tension/NewWorld/archive/master.zip #下载NewWorld主题模版压缩包
-unzip master #解压缩主题压缩包
-mv NewWorld-master/ NewWorld/ #修改主题文件夹名称
-chown -R diradmin:diradmin NewWorld/ #设置主题所有权
-rm -f master #删除主题模版压缩包
+tar -cf enhanced.tar.gz enhanced/ #备份主题目录
+cd enhanced/ 进入主题所在目录
+rm -rf * #删除文件夹内文件
+wget http://www.elinkhost.com/download/enhanced-2014-4-8.tar.gz #下载NewWorld主题模版压缩包
+tar xvzf enhanced-2014-4-8.tar.gz #解压缩
+chown -R diradmin:diradmin * #设置主题所有权
+rm -f enhanced-2014-4-8.tar.gz #删除主题模版压缩包
 exit #退出
+
