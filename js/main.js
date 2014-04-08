8afa0930b4fd2d1f64ffea4db1cab16f7cd0c52a
tension <tension@me.com>
2014-04-08 22:49:54 +0800

    更新jQuery效果引发的无法更换语言包的错误...

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 			//$("input[type=checkbox]").wrap("<label class=\"option\"></label>");
 			//$("label.option").append("<span class=\"checkbox\"></span>");
 			
-			$("select").wrap("<div class=\"select\"></div>");
+			//$("select").wrap("<div class=\"select\"></div>");
 			
 			$(".navbar-toggle").click(function() {
 				$("nav").slideToggle("5000");
